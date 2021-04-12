468ad8d5cdd24643b9240006a57572bb3a2690c2
aho71 <aho71@gatech.edu>
2021-04-12 17:24:02 -0400

    add login function

--- sli_database.py
+++ sli_database.py
@@ -3,7 +3,7 @@ import mysql.connector as mysql
 db = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = ""
+    passwd = "hoanhtuan2204"
 )
 
 print(db)
@@ -19,7 +19,7 @@ cursor.close()
 db1 = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = "",
+    passwd = "hoanhtuan2204",
     database = "sli_database"
 )
 
@@ -27,12 +27,52 @@ cursor = db1.cursor()
 
 cursor.execute("DROP TABLE IF EXISTS student")
 
-cursor.execute("CREATE TABLE student (username VARCHAR(20), password VARCHAR(20))")
+cursor.execute("CREATE TABLE student (username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20))")
 
 
 cursor.execute("DROP TABLE IF EXISTS teacher")
 
-cursor.execute("CREATE TABLE teacher (school_code int, email VARCHAR(50), password VARCHAR(20), fname VARCHAR(25), lname VARCHAR(25))")
+cursor.execute("CREATE TABLE teacher (school_code int, email VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(20), fname VARCHAR(25), lname VARCHAR(25))")
 
+
+cursor.execute("INSERT INTO student VALUES (\"student\", \"student\")")
+cursor.execute("INSERT INTO teacher VALUES (1, \"teacher\", \"teacher\", \"ABC\", \"DEF\")")
 
 cursor.close()
+
+role = input("Are you teacher(0) or student(1) ?: ")
+
+def login(role: int):
+	if role == '1':
+		while True:
+			username = input("Username: ")
+			password = input("Password: ")
+
+			cursor = db1.cursor()
+			cursor.execute("SELECT password FROM student WHERE username LIKE \"%" + str(username) + "\"")
+
+			records = cursor.fetchall()
+			if records and password in records[0]:
+				print("You are successfully login !")
+				break
+			else:
+				print("Your username or password is incorrect. Please try again !")
+
+	elif role == '0':
+		while True:
+			email = input("Email: ")
+			password = input("Password: ")
+
+			cursor = db1.cursor()
+			cursor.execute("SELECT email, password FROM teacher WHERE email LIKE \"%" + str(email) + "\"")
+
+			records = cursor.fetchall()
+			if records and password in records[0]:
+				print("You are successfully login !")
+				break
+			else:
+				print("Your email or password is incorrect. Please try again !")
+
+login(role)
+
+
