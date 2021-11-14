25a21bcb18a5c66ba38b8c268ef0855c2d5fa16d
Hannah Ward <hward9@gatech.edu>
2021-11-14 16:19:20 -0500

    changing home

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -5,7 +5,8 @@ class Home extends React.Component {
     state = {}
     render() { 
         return (
-        <div style={{position: 'absolute', left: '30%', top: '30%'}}>
+        // <div style={{position: 'absolute', left: '30%', top: '30%'}}>
+        <div>
             <h1>Seed and Lead Impact App</h1>
             <Link to="/login"><button className="btn btn-success m-2">Login</button></Link>
             <Link to="/createAccount"><button className="btn btn-primary">Create Account</button></Link>
